b2d9d551d5f39ac5ef40b1b7a747d27903b47064
itslittlejohn <itslittlejohn@gmail.com>
2017-08-14 12:36:49 -0400

    (feat): add parameter for indicating an array of actions not to buffer (#15)
    * add parameter for indicating an array of actions not to buffer
    
    * refactor to use options object instead of third argument
    
    * update README

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 var BUFFERED_ACTION_RETURN = 'redux-action-buffer: buffered action'
 
-var setImmediate = typeof global !== 'undefined' && typeof global.setImmediate !== 'undefined' ? global.setImmediate : setTimeout
+var setImmediate = typeof global !== 'undefined' &&
+  typeof global.setImmediate !== 'undefined'
+  ? global.setImmediate
+  : setTimeout
 
-module.exports = function bufferActions (breaker, cb) {
+module.exports = function bufferActions(options, cb) {
   var active = true
   var queue = []
+  var passthrough = (options && options.passthrough) || []
+  var breaker = typeof options === 'object' ? options.breaker : options
 
   var breakerType = typeof breaker
 
   if (breakerType === 'string' || breakerType === 'symbol') {
     var actionType = breaker
-    breaker = function (action) {
+    breaker = function(action) {
       if (action.type === actionType) return true
       else return false
     }
   }
 
-  return function (store) {
-    return function (next) {
-      return function (action) {
-        // console.log('next', next, action)
-        if (!active) return next(action)
+  return function(store) {
+    return function(next) {
+      return function(action) {
+        // console.log("next", next, action);
+        if (!active || passthrough.includes(action.type)) return next(action)
         if (breaker(action)) {
           active = false
           var result = next(action)
-          setImmediate(function () {
+          setImmediate(function() {
             var queueResults = []
-            queue.forEach(function (queuedAction) {
+            queue.forEach(function(queuedAction) {
               var queuedActionResult = next(queuedAction)
               queueResults.push(queuedActionResult)
             })
-            cb && cb(null, {
-              results: queueResults,
-              queue: queue
-            })
+            cb &&
+              cb(null, {
+                results: queueResults,
+                queue: queue
+              })
           })
           return result
         } else {
--- test/tests.js
+++ test/tests.js
@@ -9,7 +9,7 @@ const BREAKER = 'BREAKER'
 test('buffers actions', t => {
   var actionHistory = []
 
-  function breakCallback(err, {results, queue}) {
+  function breakCallback(err, { results, queue }) {
     // result is an object with results and queue arrays
     t.is(results.length, 2)
     t.is(queue.length, 2)
@@ -24,10 +24,10 @@ test('buffers actions', t => {
     applyMiddleware(actionBuffer(BREAKER, breakCallback))
   )
 
-  let action1 = {type: 'ACTION1'}
-  let action2 = {type: 'ACTION2'}
-  let action3 = {type: 'ACTION3'}
-  let breaker = {type: 'BREAKER'}
+  let action1 = { type: 'ACTION1' }
+  let action2 = { type: 'ACTION2' }
+  let action3 = { type: 'ACTION3' }
+  let breaker = { type: 'BREAKER' }
 
   let r1 = store.dispatch(action1)
   let r2 = store.dispatch(action2)
@@ -40,12 +40,65 @@ test('buffers actions', t => {
   t.same(rB, breaker)
   t.same(r3, action3)
 
-  setTimeout(function () {
+  setTimeout(function() {
     // history is re-ordered as expected
     t.is(actionHistory.indexOf(breaker), 0)
     t.is(actionHistory.indexOf(action1), 1)
     t.is(actionHistory.indexOf(action2), 2)
     t.is(actionHistory.indexOf(action3), 3)
+    t.pass()
+  }, 1000)
+})
+
+test('buffers actions except passthrough', t => {
+  var actionHistory = []
+
+  function breakCallback(err, { results, queue }) {
+    // result is an object with results and queue arrays
+    t.is(results.length, 3)
+    t.is(queue.length, 2)
+  }
+
+  let store = createStore(
+    (state, action) => {
+      if (action.type.indexOf('@@') !== 0) actionHistory.push(action)
+      return {}
+    },
+    null,
+    applyMiddleware(
+      actionBuffer(
+        { breaker: BREAKER, passthrough: ['PASS_THROUGH'] },
+        breakCallback
+      )
+    )
+  )
+
+  let actionp = { type: 'PASS_THROUGH' }
+  let action1 = { type: 'ACTION1' }
+  let action2 = { type: 'ACTION2' }
+  let action3 = { type: 'ACTION3' }
+  let breaker = { type: 'BREAKER' }
+
+  let rp = store.dispatch(actionp)
+  let r1 = store.dispatch(action1)
+  let r2 = store.dispatch(action2)
+  let rB = store.dispatch(breaker)
+  let r3 = store.dispatch(action3)
+
+  // buffered actions return strings, other actions return themselves
+  t.same(rp, actionp)
+  t.ok(typeof r1 === 'string')
+  t.ok(typeof r2 === 'string')
+  t.same(rB, breaker)
+  t.same(r3, action3)
+
+  setTimeout(function() {
+    // history is re-ordered as expected
+    t.is(actionHistory.indexOf(actionp), 0)
+    t.is(actionHistory.indexOf(breaker), 1)
+    t.is(actionHistory.indexOf(action1), 2)
+    t.is(actionHistory.indexOf(action2), 3)
+    t.is(actionHistory.indexOf(action3), 4)
     t.pass()
   }, 1000)
 })
